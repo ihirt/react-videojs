576b5436d81216a1a5fdbc10b7be6d2c74a6700e
Ivo Hirt <ivo.hirt@qlgroup.ch>
2022-08-05 23:15:26 +0700

    Dynamic initialization

--- src/index.js
+++ src/index.js
@@ -4,29 +4,9 @@ import Videojs from "./video.js";
 
 // from https://videojs.com/guides/react/
 
-const videoJsOptions = {
-  autoplay: false,
-  playbackRates: [0.5, 1, 1.25, 1.5, 2],
-  width: 800,
-  height: 450,
-  controls: true,
-  sources: [
-    {
-      //src: '//vjs.zencdn.net/v/oceans.mp4',
-      /*
-      src: "http://techslides.com/demos/sample-videos/small.mp4",
-      type: "video/mp4",
-      */
-      //src: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
-      src: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8",
-      type: "application/x-mpegURL",
-    },
-  ],
-};
-
 const App = () => (
-  <div>
-    <Videojs {...videoJsOptions} />
+  <div id="tvPlayer">
+    <Videojs />
   </div>
 );
 
--- src/video.js
+++ src/video.js
@@ -3,11 +3,34 @@ import React from "react";
 import videojs from "video.js";
 
 export default class Videojs extends React.Component {
+  options = {
+    autoplay: false,
+    playbackRates: [0.5, 1, 1.25, 1.5, 2],
+    //width: 800,
+    //height: 450,
+    fluid: true,
+    controls: true,
+  };
+
+  onPlayerReady() {
+    // set source dynamically
+    console.log("onPlayerReady setting source");
+    this.player.src({
+      src: "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8",
+      //src: "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8",
+      type: "application/x-mpegURL",
+    });
+    this.player.play().catch((e) => {
+      console.error("caught", e);
+    });
+    console.log("this %o, %o", this, this.player.hls);
+  }
+
   componentDidMount() {
     // instantiate video.js
-    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
-      console.log("onPlayerReady", this);
-    });
+    //this.player = videojs(this.videoNode, this.props, this.onPlayerReady.bind(this));
+    this.player = videojs("nyPlayer", this.options);
+    this.player.on("ready", this.onPlayerReady.bind(this));
 
     console.log("node", this.videoNode, this);
     if (this.videoNode) {
@@ -30,7 +53,7 @@ export default class Videojs extends React.Component {
     console.log("render");
     return (
       <div data-vjs-player>
-        <video ref={(node) => (this.videoNode = node)} className="video-js" />
+        <video id="nyPlayer" ref={(node) => (this.videoNode = node)} className="video-js" />
       </div>
     );
   }
